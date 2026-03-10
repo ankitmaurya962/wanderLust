3446fcd435f23293154dba271e4fe587c1e4ebdf
ankitmaurya962 <ankitmaurya36626@gmail.com>
2026-03-11 00:36:24 +0530

    review styled

--- schema.js
+++ schema.js
@@ -1,12 +1,18 @@
 const Joi = require('joi');
 
-const listingSchema = Joi.object({
+module.exports.listingSchema = Joi.object({
     title: Joi.string().required(),
-    description: Joi.string().required(),
+    desc: Joi.string().required(),
     location: Joi.string().required(),
     country: Joi.string().required(),
     price: Joi.string().min(0).required(),
     image: Joi.string().allow("", null)
 });
 
-module.exports = listingSchema;
+module.exports.reviewSchema = Joi.object({
+    review : Joi.object({
+        rating: Joi.number().required().min(1).max(5),
+        comments: Joi.string().required(),
+    }).required()
+})
+
